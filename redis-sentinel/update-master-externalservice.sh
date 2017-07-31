9e8799d9aa8c4df06695c828b4d5cffe7a003954
ahfeel <ahfeel@gmail.com>
2017-07-31 19:45:27 +0200

    * Tweak sentinel entrypoint and fix update master script. Last steps : bulletproof sentinel starting script and external service creation.

--- redis-sentinel/update-master-externalservice.sh
+++ redis-sentinel/update-master-externalservice.sh
@@ -4,7 +4,8 @@ REDIS_MASTER_UUID=$(curl -s http://rancher-metadata/latest/self/stack/services/r
 
 if [ $REDIS_MASTER_UUID = 'Not found' ]; then
 	# Should create master.
+	echo "Should create master"
 else
 	REDIS_MASTER_SERVICE_URL=$(curl "${CATTLE_URL}/services?uuid=${REDIS_MASTER_UUID}" -s -u "${CATTLE_ACCESS_KEY}:${CATTLE_SECRET_KEY}" | jq -r '.data[0].links.self')
-	curl "${REDIS_MASTER_SERVICE_URL}" -u "${CATTLE_ACCESS_KEY}:${CATTLE_SECRET_KEY}" -H 'Content-Type: application/json' -X PUT -d '{"externalIpAddresses":["$6"]}'
+	curl "${REDIS_MASTER_SERVICE_URL}" -u "${CATTLE_ACCESS_KEY}:${CATTLE_SECRET_KEY}" -H 'Content-Type: application/json' -X PUT -d "{\"externalIpAddresses\":[\"$6\"]}"
 fi
